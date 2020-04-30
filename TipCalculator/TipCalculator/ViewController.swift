8c586ac96790a7bdd7d99614452e3a9ea4a9b774
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2020-04-29 20:45:15 -0700

    Implement MVP for Assignment 5

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tipAmountLabel: UILabel!
+    @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipPercentageTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calculateTip() {
+        guard let amountText = billAmountTextField.text, let amount = Float(amountText) else { return }
+        guard let tipText = tipPercentageTextField.text, let tip = Float(tipText) else { return }
+        tipAmountLabel.text = "$ \(String(format: "%.2f", amount * tip / 100))"
     }
-
-
 }
-
