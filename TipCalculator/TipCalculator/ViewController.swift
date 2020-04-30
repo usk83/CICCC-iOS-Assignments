722be9af2fb4e86a6f14113d71f04f7707813fa2
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2020-04-29 21:31:46 -0700

    Improve UX

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    var amount: Float?
+    var tipPercentage: Float!
+
     @IBOutlet var tipAmountLabel: UILabel!
-    @IBOutlet var billAmountTextField: UITextField!
-    @IBOutlet var tipPercentageTextField: UITextField!
+    @IBOutlet var amountTextField: UITextField!
+    @IBOutlet var tipPercentageSlider: UISlider!
+    @IBOutlet var tipPercentageLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        amountTextField.delegate = self
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dissmissKeyboard))
+        view.addGestureRecognizer(tapGestureRecognizer)
+
+        tipPercentage = tipPercentageSlider.value
+        updateUI()
     }
 
-    @IBAction func calculateTip() {
-        guard let amountText = billAmountTextField.text, let amount = Float(amountText) else { return }
-        guard let tipText = tipPercentageTextField.text, let tip = Float(tipText) else { return }
-        tipAmountLabel.text = "$ \(String(format: "%.2f", amount * tip / 100))"
+    @objc func dissmissKeyboard(_ recognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+
+    func updateUI() {
+        tipPercentageLabel.text = "\(String(format: "%.0f", tipPercentage)) %"
+        if let amount = amount {
+            tipAmountLabel.text = "$ \(String(format: "%.2f", amount * tipPercentage / 100))"
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    @IBAction func updateAmount(_ sender: UITextField) {
+        guard let amountText = sender.text else {
+            amount = nil
+            return
+        }
+        amount = Float(amountText)
+        updateUI()
+    }
+    
+    @IBAction func updateTipPercentage(_ sender: UISlider) {
+        tipPercentage = round(sender.value)
+        sender.value = tipPercentage
+        updateUI()
     }
 }
